950d424f116fa3de52eb3ab897d87c7e72fb2ebb
Claytonmodderr <121697788+Claytonmodderr@users.noreply.github.com>
2023-09-19 23:15:36 -0300

    Update video-list.js

--- video-list.js
+++ video-list.js
@@ -11,52 +11,52 @@ let allVideos = [
    },
    {
       name: "Sinal 2",
-      src: "h#dOvLae3VlAk",
+      src: "https://tv.wcli.li/record/live.php?ch=1",
       id: "vid_3"
    },
    {
       name: "Sinal 2 ALT",
-      src: "h#li7NA395L-E",
+      src: "https://tv.wcli.li/record/alt.php?ch=1",
       id: "vid_4"
    },
    {
       name: "Sinal 3", 
-      src: "h#N-klM6y_AtY",
+      src: "https://tv.wcli.li/record/live.php?ch=2",
       id: "vid_5"
    },
    {
       name: "Sinal 3 ALT",
-      src: "h#bk7iVHlRnHU",
+      src: "https://tv.wcli.li/record/alt.php?ch=2",
       id: "vid_6"
    },
    {
       name: "Sinal 4",
-      src: "h#zO6zgt-C-vQ",
+      src: "https://tv.wcli.li/record/live.php?ch=3",
       id: "vid_7"
    },
    {
       name: "Sinal 4 ALT",
-      src: "h#osd8Lqn_oVs",
+      src: "https://tv.wcli.li/record/alt.php?ch=3",
       id: "vid_8"
    },
    {
       name: "Sinal 5",
-      src: "h#6SO2deNHoCM",
+      src: "https://tv.wcli.li/record/live.php?ch=4",
       id: "vid_9"
    },
    {
       name: "Sinal 5 ALT",
-      src: "h#hvEa8pnUjCE",
+      src: "https://tv.wcli.li/record/alt.php?ch=4",
       id: "vid_10"
    },
    {
       name: "Sinal 6",
-      src: "h#hvEa8pnUjCE",
+      src: "https://tv.wcli.li/record/live.php?ch=5",
       id: "vid_12"
    },
    {
       name: "Sinal 6 ALT",
-      src: "h#Bk3Pe5XPgUI",
+      src: "https://tv.wcli.li/record/alt.php?ch=5",
       id: "vid_13"
    },
    {
@@ -102,7 +102,7 @@ let allVideos = [
  
    {
       name: "Sinal 1 720p",
-      src: "https://reidoscanais.com/embed/?id=afazenda15",
+      src: "https://playertv.net/playplusembed/live.php?ch=0",
       id: "vid_16"
    },
    {
@@ -112,52 +112,52 @@ let allVideos = [
    },
    {
       name: "Sinal 2 720p",
-      src: "h#dOvLae3VlAk",
+      src: "https://reidoscanais.com/embed/?id=afazenda15-2",
       id: "vid_18"
    },
    {
       name: "Sinal 2 ALT",
-      src: "h#li7NA395L-E",
+      src: "https://reidoscanais.com/embed/?id=afazenda15-2",
       id: "vid_19"
    },
    {
       name: "Sinal 3 720p", 
-      src: "h#N-klM6y_AtY",
+      src: "https://reidoscanais.com/embed/?id=afazenda15-3",
       id: "vid_20"
    },
    {
       name: "Sinal 3 ALT",
-      src: "h#bk7iVHlRnHU",
+      src: "https://reidoscanais.com/embed/?id=afazenda15-3",
       id: "vid_21"
    },
    {
       name: "Sinal 4 720p",
-      src: "h#zO6zgt-C-vQ",
+      src: "https://reidoscanais.com/embed/?id=afazenda15-4",
       id: "vid_22"
    },
    {
       name: "Sinal 4 ALT",
-      src: "h#osd8Lqn_oVs",
+      src: "https://reidoscanais.com/embed/?id=afazenda15-4",
       id: "vid_23"
    },
    {
       name: "Sinal 5 720p",
-      src: "h#6SO2deNHoCM",
+      src: "https://reidoscanais.com/embed/?id=afazenda15-5",
       id: "vid_24"
    },
    {
       name: "Sinal 5 ALT",
-      src: "h#hvEa8pnUjCE",
+      src: "https://reidoscanais.com/embed/?id=afazenda15-5",
       id: "vid_25"
    },
    {
       name: "Sinal 6 720p",
-      src: "h#Bk3Pe5XPgUI",
+      src: "https://reidoscanais.com/embed/?id=afazenda15-6",
       id: "vid_26"
    },
    {
       name: "Sinal 6 ALT",
-      src: "h#Bk3Pe5XPgUI",
+      src: "https://reidoscanais.com/embed/?id=afazenda15-6",
       id: "vid_27"
    },
    {
@@ -199,5 +199,10 @@ let allVideos = [
       name: "Record TV",
       src: "http:// tv.wcli.li/record/live.php?ch=1",
       id: "vid_35"
+   },
+   {
+      name: "Record TV 720p ALT",
+      src: "https://reidoscanais.net/embed/?id=recordsp",
+      id: "vid_36"
    }
 ]
